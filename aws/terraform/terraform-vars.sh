583e6ab215355e13b39ef5d83f416fa02d170b96
Hector Rivas Gandara <keymon@gmail.com>
2017-11-06 23:27:59 +0000

    Do not query the ip to ifconfig everytime

--- aws/terraform/terraform-vars.sh
+++ aws/terraform/terraform-vars.sh
@@ -2,9 +2,11 @@ export TF_VAR_env="${DEPLOY_ENV}"
 
 export TF_VAR_aws_account_id="$(pass keytwine/aws/sandbox/account_id)"
 
-current_ip="$(curl -qs ifconfig.co)"
-
+current_ip="$(cat ../generated/current_ip.txt)"
 export TF_VAR_admin_cidrs="$(
 	pass keytwine/aws/allowed_ips.json |
 		jq -r --arg current_ip "$current_ip" ' . += [$current_ip] | unique'
 )"
+
+export TF_VAR_ssh_id_rsa="$(cat ../generated/bosh_lite.id_rsa)"
+export TF_VAR_ssh_id_rsa_pub="$(cat ../generated/bosh_lite.id_rsa.pub)"
