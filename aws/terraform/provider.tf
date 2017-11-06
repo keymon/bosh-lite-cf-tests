d0458bb68cc23f631f172abb2c07b6691bb3893a
Hector Rivas Gandara <keymon@gmail.com>
2017-11-06 22:18:21 +0000

    Add version restriction to aws provider

--- aws/terraform/provider.tf
+++ aws/terraform/provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region = "${var.aws_default_region}"
+  region              = "${var.region}"
+  version             = "~> 1.2"
   allowed_account_ids = [
     "${var.aws_account_id}",
   ]
